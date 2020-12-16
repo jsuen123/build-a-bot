9f316fa2616b32153e43cc95eb40272f69485178
JSuen <john.suen@racingauustralia.horse>
2020-12-16 14:48:34 +1100

    Added Vuex Getter - cart sale items

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -23,6 +23,29 @@
       </tr>
       </tbody>
     </table>
+    <h2 class="saleItems">You saved money on these robots:</h2>
+    <table>
+      <thead>
+      <tr>
+        <th class="robot-title">
+          Robot
+        </th>
+        <th class="cost">
+          Cost
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(robot, index) in cartSaleItems" :key="index">
+        <td class="robot-title">
+          {{ robot.head.title }}
+        </td>
+        <td class="cost">
+          {{ robot.cost }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -32,6 +55,9 @@ export default {
   computed: {
     cart() {
       return this.$store.state.cart;
+    },
+    cartSaleItems() {
+      return this.$store.getters.cartSaleItems;
     },
   },
 };
@@ -49,5 +75,11 @@ td, th {
 
 .cost {
   text-align: right;
+}
+
+.saleItems {
+  margin-top: 50px;
+  font-size: 18px;
+  color: red;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -13,4 +13,9 @@ export default new Vuex.Store({
       state.cart.push(robot);
     },
   },
+  getters: {
+    cartSaleItems(state) {
+      return state.cart.filter((item) => item.head.onSale);
+    },
+  },
 });
